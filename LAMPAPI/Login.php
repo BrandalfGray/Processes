ad68f4389a3173a4cf3fdae279b83b9708422d73
jlonge421 <119559784+jlonge421@users.noreply.github.com>
2024-08-30 03:44:26 -0400

    Working API

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,34 +1,44 @@
 <?php
-    // take incoming JSON package
+    // Enable error reporting for debugging
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    // Take incoming JSON package
     $inData = getRequestInfo();
-    // set variable to default values
+
+    // Check if JSON was parsed correctly
+    if ($inData === null) {
+        returnWithError("Invalid JSON input");
+        exit();
+    }
+
+    // Set variables to default values
     $id = 0;
     $firstName = "";
     $lastName = "";
-    // connecting API endpoint to the database
-    $conn = new mysqli("143.198.189.240", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+    // Connecting API endpoint to the database
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
     // Check connection
     if ($conn->connect_error) {
-        // die(json_encode(["error" => "Connection failed"]));
         returnWithError($conn->connect_error);
     }
     else
     {
-        $stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-        // make sure the capitalization is the same as in JSON if any errors occur
-        $stmt->bind_param("ss", $indata["login"], $inData["password"]);
+        $stmt = $conn->prepare("SELECT ID, firstName, lastName FROM Users WHERE Login = ? AND Password = ?");
+        // Make sure the capitalization is the same as in JSON if any errors occur
+        $stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if ( $row = $result->fetch_assoc() )
-        {
-            returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+        if ($row = $result->fetch_assoc()) {
+            returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
         }
-        else
-        {
+        else {
             returnWithError("No Records Found");
         }
-        $stmt->close();
+
         $stmt->close();
     }
 
@@ -37,22 +47,21 @@
         return json_decode(file_get_contents('php://input'), true);
     }
 
-    function sendResultsInfoAsJson( $obj )
+    function sendResultsInfoAsJson($obj)
     {
         header('Content-type: application/json');
         echo $obj;
     }
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-    
+    function returnWithError($err)
+    {
+        $retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+        sendResultsInfoAsJson($retValue);
+    }
+
+    function returnWithInfo($firstName, $lastName, $id)
+    {
+        $retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        sendResultsInfoAsJson($retValue);
+    }
 ?>
