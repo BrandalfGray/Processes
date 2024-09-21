aad6647e85353efaa32f0aa374c843959d11c6e7
Mistlo <96539755+Ishankp@users.noreply.github.com>
2024-09-21 16:32:10 -0400

    Add files via upload
    Updated files from server

---
+++ Login.php
@@ -0,0 +1,73 @@
+<?php
+    // Enable error reporting for debugging
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
+    // Take incoming JSON package
+    $inData = getRequestInfo();
+
+    // Check if JSON was parsed correctly
+    if ($inData === null) {
+        returnWithError("Invalid JSON input");
+        exit();
+    }
+
+    // Set variables to default values
+    $id = 0;
+    $firstName = "";
+    $lastName = "";
+
+    // Connecting API endpoint to the database
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+
+    // Check connection
+    if ($conn->connect_error) {
+        returnWithError($conn->connect_error);
+    } else {
+        $stmt = $conn->prepare("SELECT ID, firstName, lastName FROM Users WHERE Login = ? AND Password = ?");
+        $stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($row = $result->fetch_assoc()) {
+            returnWithInfo($row['firstName'], $row['lastName'], $row['ID']);
+        } else {
+            returnWithError("No Records Found");
+        }
+
+        $stmt->close();
+    }
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultsInfoAsJson($obj)
+    {
+        header('Content-type: application/json');
+        echo json_encode($obj); // Encode the array/object as JSON
+    }
+
+    function returnWithError($err)
+    {
+        $retValue = array(
+            "id" => 0,
+            "firstName" => "",
+            "lastName" => "",
+            "error" => $err
+        );
+        sendResultsInfoAsJson($retValue);
+    }
+
+    function returnWithInfo($firstName, $lastName, $id)
+    {
+        $retValue = array(
+            "id" => $id,
+            "firstName" => $firstName,
+            "lastName" => $lastName,
+            "error" => ""
+        );
+        sendResultsInfoAsJson($retValue);
+    }
+?>
